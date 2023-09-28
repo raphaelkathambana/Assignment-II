f6888cebf2fc4efa2389bef7cb23e8fff19929a3
Raphael Kathambana <maya12raph@gmail.com>
2023-09-28 12:54:26 +0300

    added login and register forms

---
+++ login.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+
+<body>
+    <h1>Enter Your Details Below</h1>
+    <form action="signIn.php" method="post" enctype="multipart/form-data">
+        <label for="email">Email:</label>
+        <input type="email" id="email" name="email" required><br><br>
+        <label for="password">Password:</label>
+        <input type="password" id="password" name="password" required>
+        <input type="checkbox" id="show-password" onclick="togglePassword()"> Show Password<br><br>
+        <input type="submit" value="Login">
+    </form>
+    <script>
+        function togglePassword() {
+            var passwordInput = document.getElementById("password");
+            if (passwordInput.type === "password") {
+                passwordInput.type = "text";
+            } else {
+                passwordInput.type = "password";
+            }
+        }
+    </script>
+</body>
+
+</html>
---
+++ register.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Register</title>
+</head>
+<body>
+    <h1>Sign Up</h1>
+    <form method="post" action="signup.php" enctype="multipart/form-data">
+        <label for="username">Username:</label>
+        <input type="text" name="username" required><br><br>
+        <label for="email">Email:</label>
+        <input type="email" name="email" required><br><br>
+        <label for="password">Password:</label>
+        <input type="password" name="password" required><br><br>
+        <input type="submit" value="Sign Up">
+    </form>
+</body>
+</html>
