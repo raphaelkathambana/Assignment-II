41124909955145bf256922ac0013f7a35869719e
Raphael Kathambana <maya12raph@gmail.com>
2023-09-29 12:07:28 +0300

    switched position of name and email

--- register.php
+++ register.php
@@ -7,10 +7,10 @@ $layout->navbar();
 ?>
     <h1>Sign Up</h1>
     <form method="post" action="signUp.php" enctype="multipart/form-data">
-        <label for="username">Username:</label>
-        <input type="text" name="username" required><br><br>
         <label for="email">Email:</label>
         <input type="email" name="email" required><br><br>
+        <label for="username">Username:</label>
+        <input type="text" name="username" required><br><br>
         <label for="password">Password:</label>
         <input type="password" name="password" required><br><br>
         <input type="submit" value="Sign Up" name="register">
